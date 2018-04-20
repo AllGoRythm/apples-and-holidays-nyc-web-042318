9159ebc721e8865b301a4f11c1c4fffa8d28cbf7
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-20 12:35:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,6 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_name = holiday.to_s.split("_").collect do |name|
         name.capitalize!
     end
+    holiday_name = holiday_name.join(" ")
   end
 
 end
