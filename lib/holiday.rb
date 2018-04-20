1e53745df9fec195b3f3864c5eed94693f730fd1
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-20 12:31:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     p "#{season.to_s.capitalize!}"
     holiday.each do |holiday, item|
-
+      holiday_name
     end
   end
 
